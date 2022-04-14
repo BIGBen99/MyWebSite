625cad11219af7961bd5a1257c164f44449aadb7
BIGBen <github@didry-taponot.fr>
2022-04-14 17:23:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,47 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mon blog</title>
-        <link href="style.css" rel="stylesheet" /> 
-    </head>
-        
-    <body>
-        <h1>Mon super blog !</h1>
-        <p>Derniers billets du blog :</p>
- 
-        <?php
-        try
-        {
-        	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
-        }
-        catch(Exception $e)
-        {
-                die('Erreur : '.$e->getMessage());
-        }
+<?php
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+}
+catch(Exception $e)
+{
+    die('Erreur : '.$e->getMessage());
+}
 
-        $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
 
-        while ($data = $req->fetch())
-        {
-        ?>
-        <div class="news">
-            <h3>
-                <?php echo htmlspecialchars($data['titre']); ?>
-                <em>le <?php echo $data['date_creation_fr']; ?></em>
-            </h3>
-            
-            <p>
-            <?php
-            echo nl2br(htmlspecialchars($data['contenu']));
-            ?>
-            <br />
-            <em><a href="#">Commentaires</a></em>
-            </p>
-        </div>
-        <?php
-        }
-        $req->closeCursor();
-        ?>
-    </body>
-</html>
+require('affichageAccueil.php');
+?>
