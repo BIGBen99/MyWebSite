05395dd7ccbac6e9fed53c400e9c54c812432ccb
BIGBen <github@didry-taponot.fr>
2022-04-14 17:25:11 +0200

    Create affichageAccueil.php

---
+++ affichageAccueil.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon blog</title>
+        <link href="style.css" rel="stylesheet" /> 
+    </head>
+        
+    <body>
+        <h1>Mon super blog !</h1>
+        <p>Derniers billets du blog :</p>
+ 
+        
+        <?php
+        while ($data = $req->fetch())
+        {
+        ?>
+        <div class="news">
+            <h3>
+                <?php echo htmlspecialchars($data['titre']); ?>
+                <em>le <?php echo $data['date_creation_fr']; ?></em>
+            </h3>
+            
+            <p>
+            <?php
+            echo nl2br(htmlspecialchars($data['contenu']));
+            ?>
+            <br />
+            <em><a href="#">Commentaires</a></em>
+            </p>
+        </div>
+        <?php
+        }
+        $req->closeCursor();
+        ?>
+    </body>
+</html>
