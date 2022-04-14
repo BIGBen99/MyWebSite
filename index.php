e50da03d06e3d7570bec26a668ccbe23d828f170
BIGBen <github@didry-taponot.fr>
2022-04-14 17:18:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,19 +24,19 @@
         // On récupère les 5 derniers billets
         $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
 
-        while ($donnees = $req->fetch())
+        while ($data = $req->fetch())
         {
         ?>
         <div class="news">
             <h3>
-                <?php echo htmlspecialchars($donnees['titre']); ?>
-                <em>le <?php echo $donnees['date_creation_fr']; ?></em>
+                <?php echo htmlspecialchars($data['titre']); ?>
+                <em>le <?php echo $data['date_creation_fr']; ?></em>
             </h3>
             
             <p>
             <?php
             // On affiche le contenu du billet
-            echo nl2br(htmlspecialchars($donnees['contenu']));
+            echo nl2br(htmlspecialchars($data['contenu']));
             ?>
             <br />
             <em><a href="#">Commentaires</a></em>
