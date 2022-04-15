51c62d72bf9d12a8cf702bdd80ec04768928fc6b
BIGBen <github@didry-taponot.fr>
2022-04-15 14:23:44 +0200

    Update posts.sql

--- database/posts.sql
+++ database/posts.sql
@@ -1,16 +1,6 @@
-START TRANSACTION;
-
 CREATE TABLE `posts` (
-    `id`            INT(11) NOT NULL,
+    `id`            INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `title`         VARCHAR(255) NOT NULL,
     `content`       TEXT NOT NULL,
     `creation_date` DATETIME NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `posts`
-    ADD PRIMARY KEY(`id`);
-
-ALTER TABLE `posts`
-  MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT;
-
-COMMIT;
