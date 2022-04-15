468b67f9a346235adc12189b34742553d4656cce
BIGBen <github@didry-taponot.fr>
2022-04-15 17:21:10 +0200

    Update errorView.php

--- view/errorView.php
+++ view/errorView.php
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Error</title>
+        <link href="style.css" rel="stylesheet" /> 
+    </head>
+        
+    <body>
+        <?= $errorMessage ?>
+    </body>
+</html>
