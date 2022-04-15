5c8878371f6c2e71444150855186118d5d206774
BIGBen <github@didry-taponot.fr>
2022-04-15 14:27:03 +0200

    Update comments.sql

--- database/comments.sql
+++ database/comments.sql
@@ -1,23 +1,11 @@
-START TRANSACTION;
-
 CREATE TABLE `comments` (
-    `id`              INT(11) NOT NULL,
+    `id`              INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `post_id`         INT(11) NOT NULL,
+    INDEX (`post_id`),
+    FOREIGN KEY(`post_id`)
+    	REFERENCES `posts`(`id`)
+        ON UPDATE CASCADE ON DELETE CASCADE,
     `author`          VARCHAR(255) NOT NULL,
     `comment`         TEXT NOT NULL,
     `comment_date`    DATETIME NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `comments`
-    ADD PRIMARY KEY(`id`),
-    ADD KEY `FK_POSTS`(`post_id`);
-
-ALTER TABLE `comments`
-    ADD FOREIGN KEY(`post_id`)
-        REFERENCES `posts`(`id`)
-        ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE `comments`
-    MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT;
-    
-COMMIT;
