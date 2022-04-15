df711c74abcc909b7754602db7d61b3e71297bd8
BIGBen <github@didry-taponot.fr>
2022-04-15 17:02:00 +0200

    Update frontend.php

--- model/frontend.php
+++ model/frontend.php
@@ -23,8 +23,7 @@ function getComments($postId) {
     return $comments;
 }
 
-function postComment($postId, $author, $comment)
-{
+function postComment($postId, $author, $comment) {
     $db = dbConnect();
     $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
     $affectedLines = $comments->execute(array($postId, $author, $comment));
