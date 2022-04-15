5038a03fb80d078066c740e58f990bb01b6eb3e5
BIGBen <github@didry-taponot.fr>
2022-04-15 17:03:59 +0200

    Update postView.php

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -23,9 +23,22 @@
 
         <h2>Commentaires</h2>
 
+        <form action="?action=addComment&id=<?= $post['id'] ?>" method="post">
+            <div>
+                <label for="author">Auteur</label><br />
+                <input type="text" id="author" name="author" />
+            </div>
+            <div>
+                <label for="comment">Commentaire</label><br />
+                <textarea id="comment" name="comment"></textarea>
+            </div>
+            <div>
+                <input type="submit" />
+            </div>
+        </form>
+
         <?php
-        while ($comment = $comments->fetch())
-        {
+        while ($comment = $comments->fetch()) {
         ?>
             <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
             <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
