e8e120bbc8e90e598adc53afda237e41c2c4d2f5
BIGBen99 <bvef@hotmail.com>
2022-04-18 15:33:32 +0200

    namespace

--- model/Manager.php
+++ model/Manager.php
@@ -3,7 +3,7 @@ namespace BIGBen\MyWebSite\Model;
 
 class Manager {
     protected function dbConnect() {
-        $db = new PDO($_SESSION['dsn'], $_SESSION['username'], $_SESSION['password']);
+        $db = new \PDO($_SESSION['dsn'], $_SESSION['username'], $_SESSION['password']);
         return $db;
     }
 }
