cb4b9be6ddbc7e5c8495e27e7366cb6432c66ce5
BIGBen99 <bvef@hotmail.com>
2022-04-18 16:16:43 +0200

    modify comment

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -41,8 +41,10 @@
         while ($comment = $comments->fetch()) {
             if(isset($commentId) && $comment['id'] == $commentId) {
         ?>
-            <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
-            <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+            <form action="?updateComment&id=<?= $comment['id'] ?>" method="post">
+                <p><input type="text" id="author" name="author" value="<?= htmlspecialchars($comment['author']) ?>" /> le <?= $comment['comment_date_fr'] ?></p>
+                <p><textarea id="comment" name="comment"><?= nl2br(htmlspecialchars($comment['comment'])) ?></textarea></p>
+            </form>
         <?php
             } else {
         ?>
