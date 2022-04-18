9580432f0484985842834059229e31295cc8b180
BIGBen99 <bvef@hotmail.com>
2022-04-18 18:19:26 +0200

    time

--- model/PostManager.php
+++ model/PostManager.php
@@ -6,7 +6,7 @@ require_once("model/Manager.php");
 class PostManager extends \BIGBen\MyWebSite\Model\Manager {
     public function getPosts() {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y %H:%i:%s\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
 
         return $req;
     }
