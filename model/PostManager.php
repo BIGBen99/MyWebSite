59a28d746cd3adda7571b46f39788ae64250c09c
BIGBen99 <bvef@hotmail.com>
2022-04-18 18:17:59 +0200

    time

--- model/PostManager.php
+++ model/PostManager.php
@@ -13,7 +13,7 @@ class PostManager extends \BIGBen\MyWebSite\Model\Manager {
 
     public function getPost($postId) {
         $db = $this->dbConnect();
-        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y %H:%i:%s\') AS creation_date_fr FROM posts WHERE id = ?');
         $req->execute(array($postId));
         $post = $req->fetch();
 
