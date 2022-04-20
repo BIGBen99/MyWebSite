46f592cd7052a057c865cb43692daa0ae984031a
BIGBen <github@didry-taponot.fr>
2022-04-20 17:49:42 +0200

    Update frontend.php

--- controller/frontend.php
+++ controller/frontend.php
@@ -65,7 +65,7 @@ function listEntities() {
 function addEntity($siren, $numeroInternedeClassement, $name, $parent_id, $address_line1, $address_line2, $address_line3, $address_zipCode, $address_city, $address_city, $address_pliNonDitribuable) {
     $entityManager = new \BIGBen\MyWebSite\Model\EntityManager();
     
-    $affectedLines = $entityManager->addEntity($siren, $numeroInternedeClassement, $name, $parent_id, $address_line1, $address_line2, $address_line3, $address_zipCode, $address_city, $address_city, $address_pliNonDitribuable)
+    $affectedLines = $entityManager->addEntity($siren, $numeroInternedeClassement, $name, $parent_id, $address_line1, $address_line2, $address_line3, $address_zipCode, $address_city, $address_city, $address_pliNonDitribuable);
 
     if ($affectedLines === false) {
         throw new Exception('Impossible d\'ajouter l'entité !');
