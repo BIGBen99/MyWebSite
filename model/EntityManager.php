dfda5f8827254dbdd0698530feb15db4f0b8ce23
BIGBen <github@didry-taponot.fr>
2022-04-20 14:08:21 +0200

    Update EntityManager.php

--- model/EntityManager.php
+++ model/EntityManager.php
@@ -1,1 +1,12 @@
 <?php
+namespace BIGBen\MyWebSite\Model;
+
+require_once('model/Manager.php');
+
+class EntityManager extends \BIGBen\MyWebSite\Model\Manager {
+    public function getEntities() {
+        $queryResult = $this->executeQuery('SELECT id, siren, numeroInternedeClassement, name, address_line1, address_line2, address_line3, address_zipCode, address_city, address_country, address_pliNonDistribuable, creation_date FROM entities ORDER BY name');
+
+        return $queryResult;
+    }
+}
