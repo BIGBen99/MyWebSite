14cac5cb2ead8df237a22a01e3e9acd44d13678f
BIGBen <github@didry-taponot.fr>
2022-04-20 14:35:20 +0200

    Update EntityManager.php

--- model/EntityManager.php
+++ model/EntityManager.php
@@ -5,7 +5,7 @@ require_once('model/Manager.php');
 
 class EntityManager extends \BIGBen\MyWebSite\Model\Manager {
     public function getEntities() {
-        $queryResult = $this->executeQuery('SELECT id, siren, numeroInternedeClassement, name, address_line1, address_line2, address_line3, address_zipCode, address_city, address_country, IF(address_pliNonDistribuable, true, false) address_pliNonDistribuable, creation_date FROM bc_entities ORDER BY name');
+        $queryResult = $this->executeQuery('SELECT id, siren, numeroInternedeClassement, name, address_line1, address_line2, address_line3, address_zipCode, address_city, address_country, address_pliNonDistribuable, creation_date FROM bc_entities ORDER BY name');
 
         return $queryResult;
     }
