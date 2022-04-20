d0014ac167220a774201264e80dbd92e722f9712
BIGBen99 <bvef@hotmail.com>
2022-04-20 21:37:17 +0200

    namespace error

--- view/View.php
+++ view/View.php
@@ -24,7 +24,7 @@ class View {
             require($file);
             return ob_get_clean();
         } else {
-            throw new Exception('Fichier ' . $file . ' introuvable');
+            throw new \Exception('Fichier ' . $file . ' introuvable');
         }
     }
 }
