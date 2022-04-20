a15bf68884e8aaf3f69207ee53f0cd6dc03e41bc
BIGBen <github@didry-taponot.fr>
2022-04-20 14:32:54 +0200

    Update listEntitiesView.php

--- view/listEntitiesView.php
+++ view/listEntitiesView.php
@@ -12,8 +12,8 @@ foreach ($entities as $entity):
     <td><?= $entity['numeroInternedeClassement'] ?></td>
     <td><?= !is_null($entity['numeroInternedeClassement'])?$entity['siren'] . $entity['numeroInternedeClassement']:"" ?></td>
     <td><?= $entity['name'] ?></td>
-    <td></td>
-    <td><?= $entity['address_pliNonDistribuable']?"X":"" ?></td>
+    <td><?= $entity['address_line1'] ?>, <?= $entity['address_line2'] ?>, <?= $entity['address_line3'] ?>, <?= $entity['address_zipcode'] ?> <?= $entity['address_city'] ?>, <?= $entity['address_country'] ?></td>
+    <td><?= $entity['address_pliNonDistribuable'] ?></td>
     <td><?= $entity['creation_date'] ?></td>
   </tr>
 <?php endforeach; ?>
