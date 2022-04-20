4a95e021c8a526c33f3a6343bfd4af671b692d8e
BIGBen <github@didry-taponot.fr>
2022-04-20 14:13:16 +0200

    Update listEntitiesView.php

--- view/listEntitiesView.php
+++ view/listEntitiesView.php
@@ -1,1 +1,23 @@
+<?php $title = 'Mon blog'; ?>
 
+<?php ob_start(); ?>
+<table>
+  <tr><td>ID</td><td>SIREN</td><td>NIC</td><td>SIRET</td><td>Raison sociale</td><td>Adresse</td><td>PND</td><td>Date de création</td></tr>
+<?php
+foreach ($entities as $entity):
+?>
+  <tr>
+    <td><?= $entity['id'] ?></td>
+    <td><?= $entity['siren'] ?></td>
+    <td><?= $entity['numeroInternedeClassement'] ?></td>
+    <td><?= $entity['siren'] ?><?= $entity['numeroInternedeClassement'] ?></td>
+    <td><?= $entity['name'] ?></td>
+    <td></td>
+    <td><?= $entity['address_pliNonDistribuable'] ?></td>
+    <td><?= $entity['creation_date'] ?></td>
+  </tr>
+<?php endforeach; ?>
+</table>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
