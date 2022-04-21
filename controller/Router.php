781a88737e6dfe9f0323a678e43a736184f1e3de
BIGBen <github@didry-taponot.fr>
2022-04-21 08:51:04 +0200

    Update Router.php

--- controller/Router.php
+++ controller/Router.php
@@ -3,15 +3,18 @@ namespace BIGBen\MyWebSite\Controller;
 
 require_once('controller/HomeController.php');
 require_once('controller/PostController.php');
+require_once('controller/EntityController.php');
 require_once('view/View.php');
 
 class Router {
     private $homeController;
     private $postController;
+    private $entityController;
 
     public function __construct() {
         $this->homeController = new \BIGBen\MyWebSite\Controller\HomeController();
         $this->postController = new \BIGBen\MyWebSite\Controller\PostController();
+        $this->entityController = new \BIGBen\MyWebSite\Controller\EntityController();
     }
 
     public function routeRequest() {
@@ -86,7 +89,9 @@ class Router {
                         } else {
                             throw new \Exception('Identifiant de billet non valide');
                         }
-                    } else {
+                    } elseif($_GET['action'] == 'listEntities') {
+                        $this->entityController->listEntities();
+                    }else {
                         throw new \Exception('Identifiant de billet non défini');
                     }
                 }else {
