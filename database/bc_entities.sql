f94272a330c4197bcf7447a6e2f1a3668ddcf4ff
BIGBen <github@didry-taponot.fr>
2022-04-21 10:37:58 +0200

    Create bc_entities.sql

---
+++ database/bc_entities.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `bc_entities` (
+    `id`                          INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `siren`                       VARCHAR(9),
+    `numeroInternedeClassement`   VARCHAR(5),
+    `name`                        VARCHAR(255) NOT NULL,
+    `address_line1`               VARCHAR(255),
+    `address_line2`               VARCHAR(255),
+    `address_line3`               VARCHAR(255),
+    `address_zipCode`             VARCHAR(255),
+    `address_city`                VARCHAR(255),
+    `address_country`             VARCHAR(255),
+    `address_pliNonDistribuable`  BOOLEAN,
+    `creation_date`               DATETIME NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
