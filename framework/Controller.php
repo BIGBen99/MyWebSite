b6be039a47ea6f2e40f9bc04d7e05f08544a7903
BIGBen <github@didry-taponot.fr>
2022-04-21 16:26:56 +0200

    Create Controller.php

---
+++ framework/Controller.php
@@ -0,0 +1,33 @@
+<?php
+namespace BIGBen\MyWebSite\Framework;
+
+require_once('Request.php');
+require_once('View.php');
+
+abstract class Controller {
+    private $action;
+    protected $request;
+  
+    public function setRequest(Request $request) {
+        $this->request = $request;
+    }
+  
+    public function executeAction($action) {
+        if(method_exists($this, $action)) {
+            $this->action = $action;
+            $this->{$this->action}();
+        } else {
+            $controllerClass = get_class($this);
+            throw new \Exception('Action ' . $action . 'non définie dans le classe ' . $controllerClass);
+        }
+    }
+  
+    public abstract function index();
+  
+    protected function generateView($viewData = array()) {
+        $controllerClass = get_class($this);
+        $controller = str_replace('Controller', '', $controllerClass);
+        $view = new \BIGBen\MyWebSite\Framework\View($this->action, $controller);
+        $view->generate($viewData);
+    }
+}
