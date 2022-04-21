7df1e1707f7009aa5d9155159f12a7cce7517aba
BIGBen <github@didry-taponot.fr>
2022-04-21 14:52:29 +0200

    Update Configuration.php

--- framework/Configuration.php
+++ framework/Configuration.php
@@ -1,1 +1,30 @@
 <?php
+namespace BIGBen\MyWebSite\Framework;
+
+class Configuration {
+    private static $parameters;
+
+    public static function get($name, $defaultValue = null) {
+        if (isset(self::getParameters()[$name])) {
+            $value = self::getParameters()[$name];
+        } else {
+            $value = $defautValue;
+        }
+        return $value;
+    }
+
+    private static function getParameters() {
+        if (self::$parameters == null) {
+            $filePath = "../prod.ini";
+            if (!file_exists($filePath)) {
+                $filePath = "../dev.ini";
+            }
+            if (!file_exists($filePath)) {
+                throw new \Exception("Aucun fichier de configuration trouvé");
+            } else {
+                self::$parameters = parse_ini_file($filePath);
+            }
+        }
+        return self::$parameters;
+    }
+}
