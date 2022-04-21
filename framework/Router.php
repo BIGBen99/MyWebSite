67fef09c64fcfefda5dddd0af240854aae8fd4d2
BIGBen <github@didry-taponot.fr>
2022-04-21 15:39:01 +0200

    Update and rename controller/Router.php to framework/Router.php

---
+++ framework/Router.php
@@ -0,0 +1,52 @@
+<?php
+namespace BIGBen\MyWebSite\Framework;
+
+require_once('Request.php');
+require_once('View.php');
+
+class Router {
+    public function routeRequest() {
+        try {
+            $request = new Request(array_merge($_GET, $_POST));
+            
+            $controller = $this->createController($request);
+            $action = $this->createAction($request);
+            
+            $controller->executeAction($action);
+        } catch(Exception $e) {
+            $this->manageError($e);
+        }
+    }
+    
+    private function createController(Request $request) {
+        $controller = "Home";
+        if($request->parameterExist('controller')) {
+            $controller = $request->getParameter('controller');
+            $controller = ucfirst(strtolower($controller));
+        }
+        
+        $controllerClass = $controller . 'Controller';
+        $controllerFile = 'controller/' . $controllerClass . '.php';
+        if(file_exists($controllerFile)) {
+            require($controllerFile);
+            $controller = new $controllerClass();
+            $controller->setRequest($request);
+            return $controller
+        } else {
+            throw new \Exception('Fichier ' . $controllerFile . ' introuvable');
+        }
+    }
+    
+    private function createAction(Request $request) {
+        $action = "index";
+        if($request->parameterExist('action')) {
+            $action = $request->getParameter('action');
+        }
+        return $action;
+    }
+    
+    private function manageError(Exception $exception) {
+        $view = new View('error');
+        $view->generate(array('errorMessage' => $exception->getMessage()));
+    }
+}
