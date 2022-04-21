9f7514aa2a5e47219af6fd20d93320db4c1639f5
BIGBen <github@didry-taponot.fr>
2022-04-21 15:19:07 +0200

    Update Request.php

--- framework/Request.php
+++ framework/Request.php
@@ -1,1 +1,22 @@
 <?php
+namespace BIGBen\MyWebSite\Framework;
+
+class Request {
+    private $parameters;
+
+    public function __construct($parameters) {
+        $this->parameters = $parameters;
+    }
+
+    public function parameterExist($name) {
+        return (isset($this->parameters[$name]) && $this->parameters[$name] != '');
+    }
+
+    public function getParameter($name) {
+        if ($this->parameterExist($name)) {
+            return $this->parameters[$name];
+        } else {
+            throw new \Exception('Paramètre ' . $name . ' absent de la requête');
+        }
+    }
+}
