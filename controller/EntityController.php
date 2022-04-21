353dfc9c4d632572d4053737c2ae94b0af5f82f6
BIGBen <github@didry-taponot.fr>
2022-04-21 17:51:50 +0200

    Update EntityController.php

--- controller/EntityController.php
+++ controller/EntityController.php
@@ -1,22 +1,22 @@
 <?php
 namespace BIGBen\MyWebSite\Controller;
 
+require_once('framework/Controller.php');
 require_once('model/EntityManager.php');
-require_once('view/View.php');
 
-class EntityController {
+class EntityController extends Controller {
     private $entityManager;
 
     public function __construct() {
         $this->entityManager = new \BIGBen\MyWebSite\Model\EntityManager();
     }
 
-    public function listEntities() {
+    public function index() {
         $entities = $this->entityManager->getEntities();
-        $view = new \BIGBen\MyWebSite\View\View("listEntities");
-        $view->generate(array('entities' => $entities));
+        $this->generateView(array('entities' => $entities));
     }
     
+    // A revoir
     public function addEntity($siren, $numeroInternedeClassement, $name, $parent_id, $address_line1, $address_line2, $address_line3, $address_zipCode, $address_city, $address_country, $address_pliNonDistribuable) {
         $affectedLines = $this->entityManager->addEntity($siren, $numeroInternedeClassement, $name, $parent_id, $address_line1, $address_line2, $address_line3, $address_zipCode, $address_city, $address_country, $address_pliNonDistribuable);
 
