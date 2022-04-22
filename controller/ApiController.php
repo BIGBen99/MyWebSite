e43aa6b95a8fb2d5763b79fb457572ba15b6ee68
BIGBen <github@didry-taponot.fr>
2022-04-22 16:00:00 +0200

    Update ApiController.php

--- controller/ApiController.php
+++ controller/ApiController.php
@@ -1,1 +1,16 @@
 <?php
+namespace BIGBen\MyWebSite\Controller;
+
+require_once('framework/Controller.php');
+
+class ApiController extends \BIGBen\MyWebSite\Framework\Controller {
+
+    public function __construct() {
+    }
+
+    public function index() {
+    }
+    
+    public function entities() {
+    }
+}
