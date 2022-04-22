15d85ec683954a3ab89b9aadba612bd5e8ab9b01
BIGBen <github@didry-taponot.fr>
2022-04-22 16:11:49 +0200

    Update ApiController.php

--- controller/ApiController.php
+++ controller/ApiController.php
@@ -2,15 +2,20 @@
 namespace BIGBen\MyWebSite\Controller;
 
 require_once('framework/Controller.php');
+require_once('model/EntityManager.php');
 
 class ApiController extends \BIGBen\MyWebSite\Framework\Controller {
-
+    private $entityManager;
+    
     public function __construct() {
+        $this->entityManager = new \BIGBen\MyWebSite\Model\EntityManager();
     }
 
     public function index() {
+        echo '<a href="/api/entities">entities</a>';
     }
     
     public function entities() {
+        $entities = $this->entityManager->getEntities();
     }
 }
