d8930b377a7d144080154ffeaaa027cf29b5a99d
BIGBen <github@didry-taponot.fr>
2022-05-11 10:18:22 +0200

    Update entities.php

--- api/entities.php
+++ api/entities.php
@@ -11,6 +11,9 @@
         getEntities($dbLink);
       }
       break;
+    case 'POST':
+      addEntities($dbLink);
+      break;
     default:
       header("HTTP/1.0 405 Method Not Allowed");
       break;
@@ -27,4 +30,25 @@
     }    
     header('Content-Type: application/json');
     echo json_encode($response, JSON_PRETTY_PRINT);
+  }
+
+  function AddProduct($dbLink) {
+    $siren = $_POST['siren'];
+    $nic = $_POST['numeroInternedeClassement'];
+    
+    echo $query="INSERT INTO entities(siren, numeroInternedeClassement, creation_date) VALUES('" . $siren . "', '" . $numeroInternedeClassement . "', NOW())";
+    $dbLink->query($query);
+/*    if(mysqli_query($conn, $query)) {
+      $response=array(
+        'status' => 1,
+        'status_message' =>'Produit ajoute avec succes.'
+      );
+    } else {
+      $response=array(
+        'status' => 0,
+        'status_message' =>'ERREUR!.'. mysqli_error($conn)
+      );
+    }*/
+    header('Content-Type: application/json');
+    echo json_encode($response);
   }
