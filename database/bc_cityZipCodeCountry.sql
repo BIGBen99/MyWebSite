99709ee10438fe760d6406cca098bec083a9adff
BIGBen <github@didry-taponot.fr>
2022-05-11 15:37:32 +0200

    Update bc_cityZipCodeCountry.sql

--- database/bc_cityZipCodeCountry.sql
+++ database/bc_cityZipCodeCountry.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE `bc_cityZipCodeCountry` (
+    `id`                          INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `city`                        VARCHAR(255),
+    `zipCode`                     VARCHAR(5),
+    `country`                     VARCHAR(255),
+    `creation_date`               DATETIME NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
