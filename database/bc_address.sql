67f7dd15f3d69e91b0f4960af5a1d93bee0bb39d
BIGBen <github@didry-taponot.fr>
2022-05-11 15:37:58 +0200

    Update bc_address.sql

--- database/bc_address.sql
+++ database/bc_address.sql
@@ -3,6 +3,6 @@ CREATE TABLE `bc_address` (
     `line1`                       VARCHAR(255),
     `line2`                       VARCHAR(255),
     `line3`                       VARCHAR(255),
-    'cityZipCodeCountry_id'       INT(11),
+    'cityZipCodeCountry_id`       INT(11),
     `creation_date`               DATETIME NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
