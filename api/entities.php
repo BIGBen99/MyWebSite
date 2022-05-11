65a1466a8ba2b2b67152f6c3f2b0a031c8be8523
BIGBen <github@didry-taponot.fr>
2022-05-11 09:54:11 +0200

    Update entities.php

--- api/entities.php
+++ api/entities.php
@@ -1,3 +1,17 @@
 <?php
   require_once('db_connect.php');
   $request_method = $_SERVER['REQUEST_METHOD'];
+
+  switch($request_method) {
+    case 'GET':
+      if(!empty($_GET["id"])) {
+        $id = intval($_GET["id"]);
+        getProducts($id);
+      } else {
+        getProducts();
+      }
+      break;
+    default:
+      header("HTTP/1.0 405 Method Not Allowed");
+      break;
+  }
