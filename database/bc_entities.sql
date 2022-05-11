5eff71b31f53256d5bd9bff21d3513c772b5ae0c
BIGBen <github@didry-taponot.fr>
2022-05-11 15:34:37 +0200

    Update bc_entities.sql

--- database/bc_entities.sql
+++ database/bc_entities.sql
@@ -4,12 +4,6 @@ CREATE TABLE `bc_entities` (
     `numeroInternedeClassement`   VARCHAR(5),
     `name`                        VARCHAR(255) NOT NULL,
     `parent_id`                   INT(11),
-    `address_line1`               VARCHAR(255),
-    `address_line2`               VARCHAR(255),
-    `address_line3`               VARCHAR(255),
-    `address_zipCode`             VARCHAR(255),
-    `address_city`                VARCHAR(255),
-    `address_country`             VARCHAR(255),
-    `address_pliNonDistribuable`  BOOLEAN,
+    `address_id`                  INT(11),
     `creation_date`               DATETIME NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
