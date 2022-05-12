bd40e415ea4479096cb3a55f423270f5bcec2ed2
BIGBen <github@didry-taponot.fr>
2022-05-12 16:12:03 +0200

    Update entities.php

--- api/entities.php
+++ api/entities.php
@@ -1,31 +1,59 @@
 <?php
   require_once('db_connect.php');
-  $request_method = $_SERVER['REQUEST_METHOD'];
-  
-  switch($request_method) {
+
+  switch($_SERVER['REQUEST_METHOD']) {
     case 'GET':
       if(!empty($_GET["id"])) {
         $id = intval($_GET["id"]);
-        getEntities($dbLink, $id);
+        getEntity($dbLink, $id);
       } else {
         getEntities($dbLink);
       }
       break;
-    case 'POST':
-      addEntity($dbLink);
-      break;
     default:
-      header("HTTP/1.0 405 Method Not Allowed");
+      header("HTTP/1.1 405 Method Not Allowed");
       break;
   }
 
-  function getEntities($dbLink, $id=0) {
+  function getEntity($dbLink, $id) {
+    $query = 'SELECT * FROM bc_entities LEFT JOIN bc_cityZipCodeCountry ON bc_entities.address_cityZipCodeCountry_id = bc_cityZipCodeCountry.id WHERE bc_entities.id = ' . $id . ' LIMIT 1';
+    foreach($dbLink->query($query) as $row) {
+      $response .= "{";
+      $response .= "\"id\": " . $row['id'];
+      if(!empty($row['siren'])) $response .= ", \"siren\": \"" . $row['siren'] . "\"";
+      if(!empty($row['numeroInternedeClassement'])) $response .= ", \"numeroInternedeClassement\": \"" . $row['numeroInternedeClassement'] . "\"";
+      if(!empty($row['siren']) && !empty($row['numeroInternedeClassement'])) $response .= ", \"siret\": \"" . $row['siren'] . $row['numeroInternedeClassement'] . "\"";
+      $response .= ", \"name\": \"" . $row['name'] . "\"";
+      if(!empty($row['address_line1'])) {
+        $response .= ", \"address\": {";
+        $response .= "\"line1\": \"" . $row['address_line1'] . "\"";
+        if(!empty($row['address_line2'])) $response .= ", \"line2\": \"" . $row['address_line2'] . "\"";
+        if(!empty($row['address_line3'])) $response .= ", \"line3\": \"" . $row['address_line3'] . "\"";
+        $response .= ", \"cityZipCodeCountry\": {";
+        $response .= "\"id\": " . $row['address_cityZipCodeCountry_id'] . ", ";
+        $response .= "\"zipCode\": \"" . $row['zipCode'] . "\", ";
+        $response .= "\"city\": \"" . $row['city'] . "\", ";
+        $response .= "\"country\": \"" . $row['country'] . "\"";
+        $response .= "}";
+        $response .= ", \"pliNonDistribuable\": " . ($row['address_pliNonDistribuable']==0?"false":"true");
+        $response .= "}";
+      }
+      if(!empty($row['parent_id'])) $response .= ", \"parentId\": " . $row['parent_id'];
+      $response .= "},\n";
+    }
+    if($response != '') {
+      $response = substr($response, 0, -2) . "\n";
+      header("HTTP/1.1 200 OK");
+      header("Content-Type: application/json");
+      echo $response;
+    } else {
+      header("HTTP/1.1 404 Not Found");
+    }
+  }
+
+  function getEntities($dbLink) {
     $query = 'SELECT bc_entities.id as id, siren, numeroInternedeClassement, name, address_line1, address_line2, address_line3, address_cityZipCodeCountry_id, zipCode, city, country ,address_pliNonDistribuable, parent_id FROM bc_entities LEFT JOIN bc_cityZipCodeCountry ON bc_entities.address_cityZipCodeCountry_id = bc_cityZipCodeCountry.id';
     $response = "[\n";
-    if($id != 0) {
-      $query .= ' WHERE bc_entities.id = ' . $id . ' LIMIT 1';
-      $response = '';
-    }
     $noWhere = false;
     if(isset($_GET['name'])) {
       $query .= ' WHERE name like "%' . $_GET['name'] . '%"';
@@ -52,15 +80,12 @@
         $query .= ' WHERE parent_id is NULL';
       }
     }
-    if($id == 0) {
-      if(isset($_GET['sortBy']) && strtoupper($_GET['sortBy']) == 'DESC') {
-        $query .= ' ORDER BY name DESC';
-      } else {
-        $query .= ' ORDER BY name ASC';
-      }
+    if(isset($_GET['sortBy']) && strtoupper($_GET['sortBy']) == 'DESC') {
+      $query .= ' ORDER BY name DESC';
+    } else {
+      $query .= ' ORDER BY name ASC';
     }
-//    echo $query;
-    foreach  ($dbLink->query($query) as $row) {
+    foreach($dbLink->query($query) as $row) {
       $response .= "{";
       $response .= "\"id\": " . $row['id'];
       if(!empty($row['siren'])) $response .= ", \"siren\": \"" . $row['siren'] . "\"";
@@ -85,27 +110,8 @@
       $response .= "},\n";
     }
     if($response != "[\n" && $response != '') $response = substr($response, 0, -2) . "\n";
-    if($id == 0) {
-      $response .= "]\n";
-    }
-    if($id == 0 || !empty($response)) {
-      header("HTTP/1.1 200 OK");
-      header("Content-Type: application/json");
-      echo $response;
-    } else {
-      header("HTTP/1.1 404 Not Found");
-    }
-  }
-
-  function AddEntity($dbLink) {
-    $siren = $_POST['siren'];
-    $nic = $_POST['numeroInternedeClassement'];
-    
-    $query="INSERT INTO bc_entities(siren, numeroInternedeClassement, creation_date) VALUES('" . $siren . "', '" . $nic . "', NOW())";
-//    echo $query;
-    if($response = $dbLink->query($query)) {
-      header("HTTP/1.1 200 OK");
-    } else {
-      header("HTTP/1.1 400 Method Not Allowed");
-    }
+    $response .= "]\n";
+    header("HTTP/1.1 200 OK");
+    header("Content-Type: application/json");
+    echo $response;
   }
