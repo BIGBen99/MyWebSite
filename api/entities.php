134cdea9b73bb3e97a4706efe7dba9a85946101e
BIGBen <github@didry-taponot.fr>
2022-06-15 16:57:24 +0200

    Update entities.php

--- api/entities.php
+++ api/entities.php
@@ -30,7 +30,7 @@
         if(!empty($row['address_line2'])) $response .= ", \"line2\": \"" . $row['address_line2'] . "\"";
         if(!empty($row['address_line3'])) $response .= ", \"line3\": \"" . $row['address_line3'] . "\"";
         $response .= ", \"zipCode\": \"" . $row['address_zipCode'] . "\"";
-        $response .= ", \"city\": \"" . $row['address_city'] . "\", ";
+        $response .= ", \"city\": \"" . $row['address_city'] . "\"";
         $response .= ", \"country\": {";
         $response .= "\"id\": " . $row['country_id'] . ", ";
         $response .= "\"name\": \"" . $row['country_name'] . "\"";
@@ -98,7 +98,7 @@
         if(!empty($row['address_line2'])) $response .= ", \"line2\": \"" . $row['address_line2'] . "\"";
         if(!empty($row['address_line3'])) $response .= ", \"line3\": \"" . $row['address_line3'] . "\"";
         $response .= ", \"zipCode\": \"" . $row['address_zipCode'] . "\"";
-        $response .= ", \"city\": \"" . $row['address_city'] . "\", ";
+        $response .= ", \"city\": \"" . $row['address_city'] . "\"";
         $response .= ", \"country\": {";
         $response .= "\"id\": " . $row['country_id'] . ", ";
         $response .= "\"name\": \"" . $row['country_name'] . "\"";
