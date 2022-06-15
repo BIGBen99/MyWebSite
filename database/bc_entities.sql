c81cee0b62fbcdb1c688f3cf85787647684eee56
BIGBen <github@didry-taponot.fr>
2022-06-15 15:11:28 +0200

    Update bc_entities.sql

--- database/bc_entities.sql
+++ database/bc_entities.sql
@@ -1,12 +1,13 @@
 CREATE TABLE `bc_entities` (
-    `code`                          INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id`                            INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `code`                          VARCHAR(255),
     `siren`                         VARCHAR(9),
     `numeroInternedeClassement`     VARCHAR(5),
     `name`                          VARCHAR(255) NOT NULL,
-    `parent_code`                   INT(11),
-    INDEX(`parent_code`),
-    FOREIGN KEY(`parent_code`)
-      REFERENCES `bc_entities`(`code`)
+    `parent_id`                     INT(11),
+    INDEX(`parent_id`),
+    FOREIGN KEY(`parent_id`)
+      REFERENCES `bc_entities`(`id`)
         ON UPDATE CASCADE ON DELETE SET NULL,
     `address_line1`                 VARCHAR(255),
     `address_line2`                 VARCHAR(255),
@@ -19,5 +20,6 @@ CREATE TABLE `bc_entities` (
       REFERENCES `bc_country`(`id`)
         ON UPDATE CASCADE ON DELETE RESTRICT,  
     `address_pliNonDistribuable`    BOOLEAN,
-    `creation_date`                 DATETIME NOT NULL
+    `creation_date`                 DATETIME NOT NULL,
+    UNIQUE UnicityConstraint (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
