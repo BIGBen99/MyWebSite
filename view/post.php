2e30ffa4be299c03511d93f56714d8bb54d199f8
BIGBen99 <bvef@hotmail.com>
2022-12-02 20:33:58 +0100

    Layout

--- view/post.php
+++ view/post.php
@@ -1,35 +1,23 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>Le blog de l'AVBN</title>
-        <link href="style.css" rel="stylesheet">
-    </head>
- 
-    <body>
-        <h1>Le super blog de l'AVBN !</h1>
-        <p><a href="index.php">Retour à la liste des billets</a></p>
+<?php $title = "Le blog de l'AVBN"; ?>
 
-        <div class="news">
-            <h3>
-                <?= htmlspecialchars($post['title']) ?>
-                <em>le <?= $post['french_creation_date'] ?></em>
-            </h3>
- 
-            <p>
-                <?= nl2br(htmlspecialchars($post['content']), false) ?>
-            </p>
-        </div>
- 
-        <h2>Commentaires</h2>
- 
-        <?php
-        foreach ($comments as $comment) {
-        ?>
-        <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['french_comment_date'] ?></p>
-        <p><?= nl2br(htmlspecialchars($comment['comment']), false) ?></p>
-        <?php
-        }
-        ?>
-    </body>
-</html>
+<?php ob_start(); ?>
+<h1>Le super blog de l'AVBN !</h1>
+<p><a href="index.php">Retour à la liste des billets</a></p>
+
+<div class="news">
+    <h3>
+        <?= htmlspecialchars($post['title']) ?>
+        <em>le <?= $post['french_creation_date'] ?></em>
+    </h3>
+    <p>
+        <?= nl2br(htmlspecialchars($post['content']), false) ?>
+    </p>
+</div>
+<h2>Commentaires</h2>
+<?php foreach ($comments as $comment) { ?>
+    <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['french_comment_date'] ?></p>
+    <p><?= nl2br(htmlspecialchars($comment['comment']), false) ?></p>
+<?php } ?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require 'layout.php' ?>
