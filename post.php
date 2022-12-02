23ce99f32ddd1372e9d30bc6b93f0013d9efbab3
BIGBen <github@didry-taponot.fr>
2022-12-02 17:38:20 +0100

    Update post.php

--- post.php
+++ post.php
@@ -1,5 +1,6 @@
 <?php
 
+require '../dev.ini';
 require 'model/model.php';
  
 if (isset($_GET['id']) && $_GET['id'] > 0) {
@@ -9,7 +10,7 @@ if (isset($_GET['id']) && $_GET['id'] > 0) {
     die;
 }
 
-$post = getPost($id);
-$comments = getComments($id);
+$post = getPost($dsn, $username, $password, $id);
+$comments = getComments($dsn, $username, $password, $id);
  
 require('view/post.php');
