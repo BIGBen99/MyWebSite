bf34607134a32db74d59d2de715fcbcc8c717334
BIGBen <github@didry-taponot.fr>
2022-12-02 09:30:50 +0100

    First split

--- index.php
+++ index.php
@@ -1,51 +1,58 @@
 <?php
   require '../dev.ini';
+
+  try {
+	  $database = new PDO($dsn, $username, $password);
+  } catch(Exception $e) {
+	  die('Erreur : '.$e->getMessage());
+  }
+
+  $statement = $database->query(
+	  "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
+  );
+$posts = [];
+while (($row = $statement->fetch())) {
+	$post = [
+    	'title' => $row['title'],
+    	'french_creation_date' => $row['date_creation_fr'],
+    	'content' => $row['content'],
+	];
+
+	$posts[] = $post;
+}
+
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
-   <head>
-  	<meta charset="utf-8">
-  	<title>Le blog de l'AVBN</title>
-  	<link href="style.css" rel="stylesheet">
-   </head>
+	<head>
+    	<meta charset="utf-8">
+    	<title>Le blog de l'AVBN</title>
+    	<link href="style.css" rel="stylesheet">
+	</head>
 
-   <body>
-  	<h1>Le super blog de l'AVBN !</h1>
-  	<p>Derniers billets du blog :</p>
+	<body>
+    	<h1>Le super blog de l'AVBN !</h1>
+    	<p>Derniers billets du blog :</p>
 
-  	<?php
-  	// Connexion à la base de données
-  	try
-  	{
-      	$bdd = new PDO($dsn, $username, $password);
-  	}
-  	catch(Exception $e){
-        	die( 'Erreur : '.$e->getMessage()   );
-  	}
-
-  	// On récupère les 5 derniers billets
-  	$req = $bdd->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
-
-  	while ($donnees = $req->fetch())
-  	{
-  	?>
-  	<div class="news">
-     	<h3>
-        	<?php echo htmlspecialchars($donnees['title']); ?>
-        	<em>le <?php echo $donnees['date_creation_fr']; ?></em>
-     	</h3>
-     	<p>
-     	<?php
-     	// On affiche le contenu du billet
-            	echo	nl2br ( htmlspecialchars( $donnees['content']), false);
-     	?>
-     	<br>
-     	<em><a href="#">Commentaires</a></em>
-     	</p>
-  	</div>
-  	<?php
-  	} // Fin de la boucle des billets
-  	$req->closeCursor();
-    ?>
-   </body>
+    	<?php
+    	foreach ($posts as $post) {
+    	?>
+        	<div class="news">
+            	<h3>
+                	<?php echo htmlspecialchars($post['title']); ?>
+                	<em>le <?php echo $post['french_creation_date']; ?></em>
+            	</h3>
+            	<p>
+                	<?php
+                	echo nl2br(htmlspecialchars($post['content']));
+                	?>
+                	<br />
+                	<em><a href="#">Commentaires</a></em>
+            	</p>
+        	</div>
+    	<?php
+    	}
+    	?>
+	</body>
 </html>
