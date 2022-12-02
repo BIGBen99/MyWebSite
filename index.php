10775b73e33644e23ef2560ab015fd226166e8c4
BIGBen <github@didry-taponot.fr>
2022-12-02 14:52:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,58 +1,22 @@
 <?php
-  require '../dev.ini';
+    require '../dev.ini';
 
-  try {
-	  $database = new PDO($dsn, $username, $password);
-  } catch(Exception $e) {
-	  die('Erreur : '.$e->getMessage());
-  }
+    try {
+        $database = new PDO($dsn, $username, $password);
+    } catch(Exception $e) {
+	die('Error : '.$e->getMessage());
+    }
 
-  $statement = $database->query(
-	  "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
-  );
-$posts = [];
-while (($row = $statement->fetch())) {
+    $statement = $database->query("SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5");
+    $posts = [];
+    while (($row = $statement->fetch())) {
 	$post = [
-    	'title' => $row['title'],
-    	'french_creation_date' => $row['date_creation_fr'],
-    	'content' => $row['content'],
+    	    'title' => $row['title'],
+    	    'french_creation_date' => $row['date_creation_fr'],
+    	    'content' => $row['content'],
 	];
-
 	$posts[] = $post;
-}
+    }
 
+    require 'templates/homepage.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="fr">
-	<head>
-    	<meta charset="utf-8">
-    	<title>Le blog de l'AVBN</title>
-    	<link href="style.css" rel="stylesheet">
-	</head>
-
-	<body>
-    	<h1>Le super blog de l'AVBN !</h1>
-    	<p>Derniers billets du blog :</p>
-
-    	<?php
-    	foreach ($posts as $post) {
-    	?>
-        	<div class="news">
-            	<h3>
-                	<?php echo htmlspecialchars($post['title']); ?>
-                	<em>le <?php echo $post['french_creation_date']; ?></em>
-            	</h3>
-            	<p>
-                	<?php
-                	echo nl2br(htmlspecialchars($post['content']), false);
-                	?>
-                	<br>
-                	<em><a href="#">Commentaires</a></em>
-            	</p>
-        	</div>
-    	<?php
-    	}
-    	?>
-	</body>
-</html>
