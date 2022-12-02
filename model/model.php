1cca89061244d948e67669ea52514d1b96cb1cf1
BIGBen <github@didry-taponot.fr>
2022-12-02 15:44:27 +0100

    Update model.php

--- model/model.php
+++ model/model.php
@@ -1,24 +1,22 @@
 <?php
-function getPosts($dsn, $username, $password) {
-	try {
-    	$database = new PDO($dsn, $username, $password);
-	} catch(Exception $e) {
-    	die('Error : '.$e->getMessage());
-	}
 
-	$statement = $database->query("SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5");
-	$posts = [];
-	while (($row = $statement->fetch())) {
+function getPosts($dsn, $username, $password) {
+    try {
+    	$database = new PDO($dsn, $username, $password);
+    } catch(Exception $e) {
+    	die('Error : ' . $e->getMessage());
+    }
+
+    $statement = $database->query("SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5");
+    $posts = [];
+    while (($row = $statement->fetch())) {
     	$post = [
-        	'title' => $row['title'],
-        	'french_creation_date' => $row['date_creation_fr'],
-        	'content' => $row['content'],
+            'title' => $row['title'],
+            'french_creation_date' => $row['french_creation_date'],
+            'content' => $row['content'],
     	];
 
     	$posts[] = $post;
-	}
-
-	return $posts;
+    }
+    return $posts;
 }
-
-?>
