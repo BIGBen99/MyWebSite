5651829dc4e8e7ef4e3f5dfe62562808d47b8d84
BIGBen <github@didry-taponot.fr>
2022-12-02 15:31:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <?php
-    require '../dev.ini';
+    require 'model/model.php';
 
-    try {
-        $database = new PDO($dsn, $username, $password);
-    } catch(Exception $e) {
-	die('Error : '.$e->getMessage());
-    }
+    $post = getPosts();
 
-    $statement = $database->query("SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5");
-    $posts = [];
-    while (($row = $statement->fetch())) {
-	$post = [
-    	    'title' => $row['title'],
-    	    'french_creation_date' => $row['date_creation_fr'],
-    	    'content' => $row['content'],
-	];
-	$posts[] = $post;
-    }
-
-    require 'templates/homepage.php';
+    require 'view/homepage.php';
 ?>
