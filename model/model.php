acea3908aa841239fe26cf819245cc293d6281c9
BIGBen <github@didry-taponot.fr>
2022-12-02 15:29:25 +0100

    Create model.php

---
+++ model/model.php
@@ -0,0 +1,27 @@
+<?php
+
+    require '../../dev.init';
+
+function getPosts() {
+	try {
+    	$database = new PDO($dsn, $username, $password);
+	} catch(Exception $e) {
+    	die('Error : '.$e->getMessage());
+	}
+
+	$statement = $database->query("SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5");
+	$posts = [];
+	while (($row = $statement->fetch())) {
+    	$post = [
+        	'title' => $row['title'],
+        	'french_creation_date' => $row['date_creation_fr'],
+        	'content' => $row['content'],
+    	];
+
+    	$posts[] = $post;
+	}
+
+	return $posts;
+}
+
+?>
