3c87be31ffb700a08b7fbaed2ba94b7ee2f9ef57
BIGBen99 <bvef@hotmail.com>
2022-12-03 09:43:22 +0100

    Error management

--- index.php
+++ index.php
@@ -6,30 +6,30 @@ require_once 'controllers/add_comment.php';
 require_once 'controllers/homepage.php';
 require_once 'controllers/post.php';
 
-if (isset($_GET['action']) && $_GET['action'] !== '') {
-	if ($_GET['action'] === 'post') {
-    	if (isset($_GET['id']) && $_GET['id'] > 0) {
-        	$id = $_GET['id'];
-
-        	post($dsn, $username, $password, $id);
-    	} else {
-        	echo 'Erreur : aucun identifiant de billet envoyé';
-
-        	die;
-    	}
-	} elseif ($_GET['action'] === 'addComment') {
-    	if (isset($_GET['id']) && $_GET['id'] > 0) {
-        	$id = $_GET['id'];
-
-        	addComment($dsn, $username, $password, $id, $_POST);
-    	} else {
-        	echo 'Erreur : aucun identifiant de billet envoyé';
-
-        	die;
-    	}
+try {
+	if (isset($_GET['action']) && $_GET['action'] !== '') {
+		if ($_GET['action'] === 'post') {
+			if (isset($_GET['id']) && $_GET['id'] > 0) {
+				$id = $_GET['id'];
+	
+				post($dsn, $username, $password, $id);
+			} else {
+				throw new Exception('Aucun identifiant de billet envoyé')
+			}
+		} elseif ($_GET['action'] === 'addComment') {
+			if (isset($_GET['id']) && $_GET['id'] > 0) {
+				$id = $_GET['id'];
+	
+				addComment($dsn, $username, $password, $id, $_POST);
+			} else {
+				throw new Exception('Aucun identifiant de billet envoyé')
+			}
+		} else {
+			throw new Exception("La page que vous recherchez n'existe pas.")
+		}
 	} else {
-    	echo "Erreur 404 : la page que vous recherchez n'existe pas.";
+		homepage($dsn, $username, $password);
 	}
-} else {
-	homepage($dsn, $username, $password);
+} catch (Exception $e) {
+	echo 'Erreur : '.$e->getMessage();
 }
