72a0be2e3b12afc3a5bbd72d21325e9651e082ae
BIGBen <github@didry-taponot.fr>
2022-12-07 14:00:08 +0100

    Update homepage.php

--- controllers/homepage.php
+++ controllers/homepage.php
@@ -5,6 +5,7 @@ require_once 'model/post.php';
 
 function homepage(string $dsn, string $username, string $password) {
     $postRepository = new PostRepository();
+    $postRepository->connection = new DatabaseConnection();
     $posts = $postRepository->getPosts($dsn, $username, $password);
 
     require 'views/homepage.php';
