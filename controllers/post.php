3889e3a7e95f164ab5f94424ff3ea6399fbe402f
BIGBen <github@didry-taponot.fr>
2022-12-07 14:18:40 +0100

    Update post.php

--- controllers/post.php
+++ controllers/post.php
@@ -6,8 +6,12 @@ require_once 'model/comment.php';
 
 function post(string $dsn, string $username, string $password, int $id) {
     $postRepository = new PostRepository();
+    $commentRepository = new CommentRepository();
+    
     $postRepository->connection = new DatabaseConnection();
     $post = $postRepository->getPost($dsn, $username, $password, $id);
+    
+    $commentRepository->connection = new DatabaseConnection();
     $comments = getComments($dsn, $username, $password, $id);
 
     require 'views/post.php';
